d29e58b134010ad8c0899032c405c04493ba0e3a
Ronit Roushan <ronit21102@gmail.com>
2024-03-22 10:35:04 +0530

    card using user

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
-import React from "react";
-import {BrowserRouter as Router,Route,Redirect, Switch} from 'react-router-dom'
-import UserDemo from "./users/pages/User";
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom';
 
- const App = ()=> {
-  
+import Users from './users/pages/Users';
+import NewPlace from './places/pages/NewPlace';
 
+const App = () => {
   return (
     <Router>
       <Switch>
-      <Route path="/users" >
-            <UserDemo/>
-       </Route>
-
-       <Redirect to="/" exact/>
-         
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+        <Redirect to="/" />
       </Switch>
-     
-      
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
